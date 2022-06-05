81d54d67abf0be3ddb44a91fceb97ab1c7e3279a
aryanmega12 <106868248+aryanmega12@users.noreply.github.com>
2022-06-05 21:07:27 +0430

    Create filtr-joker.py

---
+++ filtr-joker.py
@@ -0,0 +1,197 @@
+import os
+# aryan-mega-trn
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#aryan-mega-trn
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# aryan-mega-trn
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.YELLOW + "")
+x = """ mojtaba cassifer bot running...
+
+ _   _    _     _________      _  _____
+| | | |  / \   |__  /  _ \    / \|_   _|
+| |_| | / _ \    / /| |_) |  / _ \ | |
+|  _  |/ ___ \  / /_|  _ <  / ___ \| |
+|_| |_/_/   \_\/____|_| \_\/_/   \_\_|
+
+ _     ___  ____  ____
+| |   / _ \|  _ \|  _ \
+
+| |  | | | | |_) | | | |
+| |__| |_| |  _ <| |_| |
+|_____\___/|_| \_\____/
+     _  ___  _  _______ ____
+    | |/ _ \| |/ / ____|  _ \
+    
+ _  | | | | | ' /|  _| | |_) |
+| |_| | |_| | . \| |___|  _ <
+ \___/ \___/|_|\_\_____|_| \_\
+
+
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣶⣶⣶⣶⣤⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠉⠉⠉⠉⠉⠙⢢⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢘⡉⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⢐⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠳⡦⣸⣷⣄⡀⢀⣀⡀⠀⠀⠀⡂⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡊⣿⣿⣿⣿⣯⡩⣉⠹⢷⢦⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⠀⢻⣿⣿⣿⣿⣿⣿⣶⣶⡞⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡌⠀⠀⢀⠉⠻⣿⣿⣿⣿⣿⡇⠀⠀⠀
+⠀⣀⠠⠤⠐⠚⠱⠀⠀⠀⠈⠀⠀⠀⠉⣻⠛⠋⠀⠀⠀⠀
+⡇⠀⠀⡊⠠⠐⠁⠀⠀⠀⠀⢰⠀⢀⠆⠿⡀⠀⠀⠀⠀⠀
+⡗⠒⠒⠀⠀⠀⠠⠤⢤⡀⠀⢸⠀⠘⠀⠀⢌⠑⢢⠀⠀⠀
+⡇⠠⠀⠀⠀⠀⠀⠀⠀⠀⠑⢺⠀⠐⠀⠀⠂⠀⠀⠉⠒⠢⣄ 
+⡇⠀⠀⠀⠀⠀⠀⠂⢰⠤⠀⠀⢦⠁⠀⠀⢂⠀⠀⠐⠄⠀⠠⠈⠉⠑⠦⡀
+⡇⠀⡀⢀⠐⡀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠁⠄⠀⠀⠘
+ """
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# Mjtba *_*
+#my cat
+#   ️❤️❤️❤️❤️❤️❤️
+#   💙💙💙💙💙💙      
+#   💚💚💚💚💚💚  
+#   💜💜💜💜💜💜
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("ok welcome friends . ❤️ . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA @Leader_lord")
+print ("                         ID telegram @Leader_lord")
+print ("                          New Id rubika : @Lockader_Adidas")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#_________")
+time.sleep(0.6)
+print ("##________")
+time.sleep(0.5)
+print ("###_______")
+time.sleep(0.4)
+print ("####______")
+time.sleep(0.3)
+print ("#####_____")
+time.sleep(0.2)
+print ("######____")
+time.sleep(0.1)
+print ("#######___")
+time.sleep(0.5)
+print ("########__")
+time.sleep(0.1)
+print ("#########_")
+time.sleep(0.5)
+print ("##########")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input(" 2 channel & 3 for group & 4 for ac > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print("""
+  update =
+$        cd aryan-mega-trn
+$       python Filtr-jokr.py
+$       git clone https://github.com/aryanmega12/aryan-mega-trn
+""")
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '%s%s(https://xexx.4.7.4.7.4.6.3.7.9.6.8.4.8.5.7.8.5.5.7.8.4.5.5.4.6.4.7.9.5.7.9.5.6.8.5.7.9)%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s(https://.4.6.3.6.4.6.8.3.5.8.4.7.9.4.2.8.3.7.9.5.8.0.¥./8.2.0.9/f////g.h/5.1.5.8.8.0.1.4.0.6.1.5.0.7.3.7.0.4.6.8.6/)%s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("error - lotfan dorost vared konid")
+
+
+if up == "4":
+
+        print ('code for account ≠>' + '%s%s (5.6.3.6.7.4.7.4.6.7.4.6.8.4.7.7.x.e.x.e.xx4.6.7.6.7.4.6.7.4.3.5.7.4.7.8.4.3.6)%s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("error - lotfan dorost vared konid")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubikam =
+aryan-mega-trn
+aryan-mega-trn
+ """)
+print("""
+link gap rubikam ==>>>@megatrn
+""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Bye Bye My Friend :)🙂❤️")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Baraye Inke Yeki Ro Filter Konid In Ro Befrestin' + '%s%s  /  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""va belak konin va code ro ( 1 ) bar bezanin to gozaresha
+baraye account""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd $HOME")
+os.system("rm -rf aryan-mega-trn&& git clone https://github.com/aryanmega12/aryan-mega-trn&&cd aryan-mega-trn&&python Filtr-joker.py")
+print ("time end :")
+print (Fore.WHITE + "")
+date()
